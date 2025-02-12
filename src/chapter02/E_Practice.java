c8eae49d7470d4cd5da03bf349853d96b980e981
devgiants75 <devgiants75@naver.com>
2025-02-12 16:24:00 +0900

    20250211 java control practice

--- src/chapter02/E_Practice.java
+++ src/chapter02/E_Practice.java
@@ -1,5 +1,25 @@
 package chapter02;
 
 public class E_Practice {
+	public static void main(String[] args) {
+		/*
+			1. 점수에 따른 학점 출력 (if, else if, else 사용)
 
+	        사용자로부터 점수를 입력받아 아래 기준에 따라 학점을 출력하세요.
+	        0 미만 또는 100 초과인 경우: 유효하지 않은 점수입니다.
+	        90 이상: A
+	        80 이상: B
+	        70 이상: C
+	        60 이상: D
+	        그 외: F
+		
+			2. 삼각형 모양의 별(*) 출력 (중첩 for 사용)
+	        (1)
+	        *
+	        **
+	        ***
+	        ****
+	        *****
+		*/
+	}
 }
