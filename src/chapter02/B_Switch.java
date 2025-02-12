01e0f3c65ad831e968f51ed3fad6d5a81b18ee5a
devgiants75 <devgiants75@naver.com>
2025-02-12 16:23:38 +0900

    20250211 java control practice

--- src/chapter02/B_Switch.java
+++ src/chapter02/B_Switch.java
@@ -47,5 +47,56 @@ public class B_Switch {
 		default:
 			System.out.println("유효한 chapter가 아닙니다.");
 		}
+		
+		sc.nextLine(); // 개행 문자(Enter) 값 처리
+		
+		System.out.println("과일의 이름을 입력해주세요.");
+		String fruit = sc.nextLine();
+		
+		switch (fruit) {
+		case "banana":
+			System.out.println("노란색");
+			break;
+		case "orange":
+			System.out.println("주황색");
+			break;
+		case "grape":
+			System.out.println("보라색");
+			break;
+		default:
+			System.out.println("유효한 과일이 아닙니다.");
+		}
+		
+		sc.close(); // 입력기.close();
+		
+		// === else if VS switch === //
+		// 1. else if
+		// : 범위 조건식 
+		// >> 앞선 조건을 모두 배제하고 남은 데이터에서 조건 검색
+		
+		int score = 85;
+		if (score >= 90) {
+			System.out.println("우수한 성적입니다.");
+		} else if (score >= 70) {
+			System.out.println("평균적인 성적입니다.");
+		} else {
+			System.out.println("조금 더 노력해봅시다.");
+		}
+		
+		// 2. switch
+		// : 특정 값(정수, 문자, 문자열 등)을 기준으로 여러 경우(case)를 비교할 때 유용
+		String grade = "B";
+		switch (grade) {
+		case "A":
+		case "B":
+			System.out.println("우수한 성적입니다.");
+			break;
+		case "C":
+		case "D":
+			System.out.println("평균적인 성적입니다.");
+			break;
+		default:
+			System.out.println("조금 더 노력해봅시다.");
+		}
 	}
 }
