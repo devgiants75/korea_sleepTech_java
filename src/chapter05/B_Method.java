16d85805447cfd60864d6ff749cec7d9577c1bc2
devgiants75 <devgiants75@naver.com>
2025-02-14 12:27:14 +0900

    20250214 java oop method

---
+++ src/chapter05/B_Method.java
@@ -0,0 +1,28 @@
+package chapter05;
+
+/*
+	== 함수(Function) VS 메서드(Method) ==
+	: 자바에서는 "함수"의 개념 X
+	: 모든 함수 체계는 "클래스 내부"에 존재 >> "메서드"로 불림
+	
+	1. 함수
+	: 특정 작업을 수행하는 코드 블록
+	- 독립적으로 존재, 클래스 소속 X
+	- 직접 호출: 함수명();
+	>> Python, JavaScript
+	
+	2. 메서드
+	: 객체의 동작을 정의한 코드 블록
+	- 클래스 내부에 포함, 객체의 행위와 연결
+	- 객체 생성 후, .연산자를 통해 호출
+	>> Java, C#
+	
+	== 메서드 ==
+	
+*/
+
+public class B_Method {
+	public static void main(String[] args) {
+		
+	}
+}
