00994178bba86f3356b90392344209cc9a9436fb
devgiants75 <devgiants75@naver.com>
2025-02-18 16:11:19 +0900

    20250218 oop interface 예제

--- src/chapter08/F_Practice.java
+++ src/chapter08/F_Practice.java
@@ -71,12 +71,42 @@ class Mango implements TropicalFruit {
 public class F_Practice {
 	public static void main(String[] args) {
 		
+		// 다형성 적용
+		Fruit apple = new Apple();
+		Fruit orange = new Orange();
+		Fruit banana = new Banana();
+		TropicalFruit mango = new Mango();
 		
+		// 업캐스팅
+		// : mango가 Fruit 타입으로 형 변환 
+		// > TropicalFruit가 Fruit를 상속받은 인터페이스
+		Fruit[] fruits = { apple, orange, banana, mango };
 		
+		for (Fruit fruit: fruits) {
+			fruit.describe();
+			// fruit.tropicalFeature();
+			
+			if (fruit instanceof TropicalFruit) {
+				// 다운 캐스팅
+				TropicalFruit tropicalFruit = (TropicalFruit) fruit;
+				tropicalFruit.tropicalFeature();
+			}
+		}
 		
+		// A instanceof B
+		// : 'A' 객체가 B 클래스(종류)와 같은지 확인
+		class 동물 {}
+		class 고양이 extends 동물 {}
+		class 강아지 extends 동물 {}
 		
+		동물 애니멀 = new 동물();
+		동물 나비 = new 고양이();
+		동물 초코 = new 강아지();
 		
+		if (애니멀 instanceof 고양이) {
+			System.out.println("실행되지 않음!! 부모 객체는 자식 객체에 담을 수 없음!");
+		}
 		
-		
+		Fruit.printType(); // 과일(Fruit)의 타입입니다. - 정적 메서드(인터페이스로 호출)
 	}
 }
