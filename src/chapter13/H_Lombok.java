9664003e4f0f84dbe89f1fcaca041df6830f92f7
devgiants75 <devgiants75@naver.com>
2025-02-24 15:11:16 +0900

    20250224 lombok annotation

--- src/chapter13/H_Lombok.java
+++ src/chapter13/H_Lombok.java
@@ -41,15 +41,22 @@ import lombok.*; // 롬복 라이브러리 전체 클래스를 import
 
 // 1) @NoArgsConstuctor
 // 2) @AllArgsConstuctor
-// 3) @RequiredArgsConstuctor
+// 3) @RequiredArgsConstructor
 
 // 4. @Data 어노테이션
-// : @Setter, @Getter, @RequiredConstructor, @EqualsAndHashCode 어노테이션을 
+// : @Setter, @Getter, @ToString, @RequiredArgsConstructor, @EqualsAndHashCode 어노테이션을 
 //		한 번에 적용하는 어노테이션
 
 // cf) @EqualsAndHashCode
 // : 두 객체의 내용이 같은지(동등성) 비교, 두 객체가 같은 객체인지(동일성) 비교 연산자
 // >> boolean 값 반환
+
+// 5. @Builder 어노테이션
+// : 복잡한 객체를 생성할 때 사용하는 빌더 패턴을 자동으로 구현
+// - @Builder 어노테이션을 사용하는 경우
+//		>> 필드값을 모두 메서드 체이닝으로 가져옴 (선택 필드의 메서드명이 필드명)
+//		>> 필수값을 builder()의 메서드 인자로 받지 X
+//			: 메서드 체이닝에서 누락 시 오류
 
 @Data
 // @Setter
@@ -59,14 +66,15 @@ import lombok.*; // 롬복 라이브러리 전체 클래스를 import
 // : 필드에 final값이 있는 경우 빈 생성자 호출이 불가!
 @AllArgsConstructor // - title, author 모두를 매개변수로 가짐
 // @RequiredArgsConstructor // - title(필수값)을 매개변수로 가짐
+@Builder
 class LombokClass {
 	
 	// cf) @RequiredConstructor
 	//		: 필수값을 매개변수로 받는 생성자
 	//		- final 필드값 또는 @NonNull 어노테이션을 매개변수로 받음
 	
-	@NonNull // null 값이 들어갈 수 없음! (필드에 필수값 지정)
 	private final String title;
+	@NonNull // null 값이 들어갈 수 없음! (필드에 필수값 지정)
 	private String author;
 	
 	// 1. getter/setter
@@ -75,15 +83,7 @@ class LombokClass {
 	// - setter는 필드의 타입과 필드명의 매개변수를 가지는 메서드
 	// public String getTitle() { return title; }
 	// public void setAuthor(String author) { this.author = author; }
-	
-
 }
-
-
-
-
-
-
 
 public class H_Lombok {
 	public static void main(String[] args) {
@@ -93,23 +93,22 @@ public class H_Lombok {
 //		System.out.println(lombokClass);
 		// LombokClass(title=백설공주, author=이도경)
 		
-		LombokClass lombokClass1 = new LombokClass("백설공주");
+		// LombokClass lombokClass1 = new LombokClass("백설공주");
 		// lombokClass1.setTitle("신데렐라");
 		// : @Setter는 final 필드에 대해서 메서드 생성 X
 		
 		LombokClass lombokClass2 = new LombokClass("신데렐라", "이승아");
 		
-		System.out.println(lombokClass1); // LombokClass(title=백설공주, author=null)
+		// System.out.println(lombokClass1); // LombokClass(title=백설공주, author=null)
 		System.out.println(lombokClass2); // LombokClass(title=신데렐라, author=이승아)
 		
+		LombokClass lombokClass3 = LombokClass.builder()
+				.author("루피")
+				.build();
+		// author 필드는 @NonNull 지정으로 반드시 생성 시 값의 할당이 이루어져야 함!
 		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(lombokClass3);
+		// LombokClass(title=null, author=루피)
+		// : final 필드는 참조 데이터 타입의 기본값으로 null이 포함 가능
 	}
 }
