db2263a83e8539d9ad5bd5fa8e7df206fa94b4b3
devgiants75 <devgiants75@naver.com>
2025-02-27 12:48:24 +0900

    20250226 enum practice

---
+++ src/chapter16/practice/service/LibraryManager.java
@@ -0,0 +1,16 @@
+package chapter16.practice.service;
+
+import java.util.List;
+
+import chapter16.practice.entity.Category;
+import chapter16.practice.entity.Item;
+
+public class LibraryManager implements Manageable {
+
+	@Override
+	public List<Item> searchByCategory(Category category) {
+
+		return null;
+	}
+
+}
